bd30c561df7bdcd02b2f1ded4cfaddf1ff4fe769
hmy2001 <harutowii@yahoo.co.jp>
2019-08-18 02:03:42 +0900

    Fixed limited byte problem(Fixed #115)
    (cherry picked from commit c70f5ed73e56b5749701c502bb9bfb0368f93bf5)

--- src/shoghicp/BigBrother/network/Translator.php
+++ src/shoghicp/BigBrother/network/Translator.php
@@ -2483,7 +2483,7 @@ class Translator{
 
 							$pk->players[] = [
 								$entry->uuid->toBinary(),
-								TextFormat::clean($displayName),
+								substr(TextFormat::clean($displayName), 0, 16),
 								$properties,
 								$gameMode,
 								0,
